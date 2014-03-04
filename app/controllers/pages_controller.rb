dcc2a5bd8d4b08f76c257c55b1fafd373b8fffd9
Arjun Venkataswamy <avenkat2@gmail.com>
2014-03-04 07:55:24 -0600

    reverses ordering on home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def home
     if session[:user_id]
       @user = User.find(session[:user_id])
-      @recent_roadblocks = Roadblock.limit(10)
+      @recent_roadblocks = Roadblock.order(created_at: :desc).limit(10)
     else
       redirect_to "/login"
     end
