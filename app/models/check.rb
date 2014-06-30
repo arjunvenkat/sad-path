485b894ea9a3e694b1be100fc9786240b9956d2d
Arjun Venkataswamy <arjun@example.com>
2014-06-29 19:05:45 -0500

    streamlines new checklist form

--- app/models/check.rb
+++ app/models/check.rb
@@ -1,4 +1,6 @@
 class Check < ActiveRecord::Base
   has_many :roadblock_checks, dependent: :destroy
   has_many :roadblocks, through: :roadblock_checks
+
+  scope :instructor_created, -> { where(instructor_created: true)}
 end
